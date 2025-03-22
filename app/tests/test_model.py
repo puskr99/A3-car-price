2bd7fef3ad683ca8891027bcc3b98d2e935fa677
puskr99 <puskaradhikari65@gmail.com>
2025-03-22 19:02:23 +0700

    updated tests

--- app/tests/test_model.py
+++ app/tests/test_model.py
@@ -2,37 +2,25 @@ import os
 import joblib
 import pandas as pd
 
+test = pd.DataFrame([[6.42, 10, 120000, 16, 1298, 88]],
+                    columns=['name', 'year', 'km_driven', 'mileage', 'engine', 'max_power'])
+model_path = "app/model/A1-car_price_predictor"
+
 
 def test_input():
-    model_path = "app/model/A1-car_price_predictor"
-    print("I am here")
-
     assert os.path.exists(model_path), f"Model file not found: {model_path}"
 
     model = joblib.load(model_path)
-
-    # Test data
-    test = pd.DataFrame([[6.42, 10, 120000, 16, 1298, 88]],
-                         columns=['name', 'year', 'km_driven', 'mileage', 'engine', 'max_power'])
-    
     try:
         predicted_value = model.predict(test)
     except:
         assert(False, "Shape is incorrect.")
 
 
-
 def test_predicted_shape():
-    model_path = "app/model/A1-car_price_predictor"
-    print("I am here")
-
     assert os.path.exists(model_path), f"Model file not found: {model_path}"
 
     model = joblib.load(model_path)
-
-    # Test data
-    test = pd.DataFrame([[6.42, 12, 120000, 16, 1298, 88]],
-                         columns=['name', 'year', 'km_driven', 'mileage', 'engine', 'max_power'])
     
     predicted_value = model.predict(test)
 
