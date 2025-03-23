e57587f6b908433cd2ddcaf516f3c97dffd020bb
puskr99 <puskaradhikari65@gmail.com>
2025-03-23 15:08:20 +0700

    added some comments in the code

--- app/tests/test_model.py
+++ app/tests/test_model.py
@@ -2,9 +2,13 @@ import os
 import joblib
 import pandas as pd
 
+model_path = "app/model/car_price_predictor"
+scalar_model_path  = "app/model/scaler.pkl"
+
 test = pd.DataFrame([[6.42, 10, 120000, 16, 1298, 88]],
                     columns=['name', 'year', 'km_driven', 'mileage', 'engine', 'max_power'])
-model_path = "app/model/A1-car_price_predictor"
+
+scaler_model = joblib.load(scalar_model_path)
 
 
 def test_input():
@@ -12,7 +16,8 @@ def test_input():
 
     model = joblib.load(model_path)
     try:
-        predicted_value = model.predict(test)
+        data = scaler_model.fit_transform(test)
+        predicted_value = model.predict(data)
     except:
         assert(False, "Shape is incorrect.")
 
@@ -21,8 +26,12 @@ def test_predicted_shape():
     assert os.path.exists(model_path), f"Model file not found: {model_path}"
 
     model = joblib.load(model_path)
-    
-    predicted_value = model.predict(test)
 
-    assert predicted_value[0] > 0
+    data = scaler_model.fit_transform(test)
+
+    predicted_value = model.predict(data)
+
+    #predicted values can only be 0-3.
+    assert predicted_value[0] in [0, 1, 2, 3]
+    
     assert predicted_value.shape == (1,)
